a0534badc1161a683f7537df94ef720f04184289
ryo08271154 <ryo20080827@gmail.com>
2025-07-14 07:48:12 +0900

    埋め込みHTMLのサイズを修正し他のサイトも対応

--- titles/utils/embed.py
+++ titles/utils/embed.py
@@ -15,7 +15,14 @@ def generate_embed_html(text) -> list:
             elif "youtube.com/playlist?list=" in url:
                 playlist_id = url.split("?list=")[1]
             if video_id:
-                embed_html.append(f'<iframe width="300" height="200" src="https://www.youtube.com/embed/{video_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
+                embed_html.append(f'<iframe width="320" height="180" src="https://www.youtube.com/embed/{video_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
             elif playlist_id:
-                embed_html.append(f'<iframe width="300" height="200" src="https://www.youtube.com/embed/videoseries?list={playlist_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
+                embed_html.append(f'<iframe width="320" height="180" src="https://www.youtube.com/embed/videoseries?list={playlist_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
+        if "nicovideo.jp/" in url:
+            if "nicovideo.jp/watch/" in url:
+                video_id = url.split("/")[4]
+                embed_html.append(f'<script type="application/javascript" src="https://embed.nicovideo.jp/watch/{video_id}/script?w=320&h=180"></script><noscript><a href="https://www.nicovideo.jp/watch/{video_id}">https://www.nicovideo.jp/watch/{video_id}</a></noscript>')
+            if "ch.nicovideo.jp/ch" in url:
+                channel_id = url.split("/")[3]
+                embed_html.append(f'<iframe src="https://ch.nicovideo.jp/{channel_id}/thumb_channel" width="312" height="176" frameborder="0" scrolling="no"></iframe>')
     return embed_html
