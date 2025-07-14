7a1cc725e3bd7b1728ebf0e18a0e1dcfa86ea92f
ryo08271154 <ryo20080827@gmail.com>
2025-07-14 21:52:59 +0900

    URLホスト名の解析を追加

--- titles/utils/embed.py
+++ titles/utils/embed.py
@@ -1,4 +1,5 @@
 import re
+from urllib.parse import urlparse
 def generate_embed_html(text) -> list:
     urls = re.findall(r"https?://[\w/:%#$&?()~.=+-]+", text)
     if not urls:
@@ -7,7 +8,8 @@ def generate_embed_html(text) -> list:
     for url in urls:
         video_id = None
         playlist_id = None
-        if "youtube.com/" in url or "youtu.be/" in url:
+        host=urlparse(url).netloc
+        if host.endswith("youtube.com") or host.endswith("youtu.be"):
             if "youtube.com/watch?v=" in url:
                 video_id = url.split("?v=")[1][:12] #動画idは必ず11桁
             elif "youtu.be/" in url:
@@ -18,7 +20,7 @@ def generate_embed_html(text) -> list:
                 embed_html.append(f'<iframe width="320" height="180" src="https://www.youtube.com/embed/{video_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
             elif playlist_id:
                 embed_html.append(f'<iframe width="320" height="180" src="https://www.youtube.com/embed/videoseries?list={playlist_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
-        if "nicovideo.jp/" in url:
+        if host.endswith("nicovideo.jp") or host.endswith("nico.ms"):
             if "nicovideo.jp/watch/" in url:
                 video_id = url.split("/")[4]
                 embed_html.append(f'<script type="application/javascript" src="https://embed.nicovideo.jp/watch/{video_id}/script?w=320&h=180"></script><noscript><a href="https://www.nicovideo.jp/watch/{video_id}">https://www.nicovideo.jp/watch/{video_id}</a></noscript>')
